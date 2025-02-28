10353b234775fe5b828ac1add92edc27b4cc62f3
Thomas S. <thomas.soldwedel@gmail.com>
2025-02-28 14:48:59 +0100

    trying to get it running on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      'lucide-react': 'lucide-react/dist/esm/lucide-react'
-    }
-  }
-})
+    dedupe: ["lucide-react"], // Ensures only one instance of lucide-react is used
+  },
+});
